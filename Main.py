fcb553586a9f857001ab7356ae69e8d513196967
anamm8 <ana.arturcosta@gmail.com>
2022-07-10 13:34:53 +0100

    install extensões

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ print('teste 456 github Ana Maria')
 print('Será que agora funciona?')
 #https://www.kaggle.com/code/adinishad/95-accuracy-chest-x-ray-images-pneumonia
 #https://www.kaggle.com/code/adinishad/95-accuracy-chest-x-ray-images-pneumonia
+#print('Ola ANA :D teste a ver se consegues ver isto')
 
-
-print('Ola ANA :D teste a ver se consegues ver isto')
+'''Temos de fazer import destas extensões: numpy (linear algebra), pandas (data processing) e cv2
+no terminal coloquei->   pip install pandas       e repeti com os outros
+'''
